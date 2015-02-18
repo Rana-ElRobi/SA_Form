954a4a18b6e8e497313b8462da5bd103d8997b5a
Rana-ElRobi <rana.saad.abdelhalim@fcis.asu.edu.eg>
2015-02-18 20:26:34 +0200

    added methods in users controller

--- form/app/controllers/useres_controller.rb
+++ form/app/controllers/useres_controller.rb
@@ -1,5 +1,19 @@
 class UseresController < ApplicationController
-def new
-	
-end
+	def new
+		
+	end
+	def create
+		@user = User.new(user_params);
+
+		@user.save
+		redirect_to @user
+	end
+	def show
+		@user= User.find(params[:id])
+	end
+private 
+	def user_params
+		params.require(:user).permit(:name,:team)
+		
+	end
 end
--- form/db/schema.rb
+++ form/db/schema.rb
@@ -11,6 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150218180953) do
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.boolean  "taken"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.boolean  "team"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
