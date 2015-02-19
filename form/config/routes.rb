d9d79a2053e2f44966ace312117df38642a073ee
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2015-02-19 03:20:31 +0200

    Polish

--- form/config/routes.rb
+++ form/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  root 'welcome#index'
   resources :users, only: [:index, :new, :delete, :update]
   resources :projects do
     get "subscribe", on: :member
   end
+  root "projects#index"
 end
